66ea4c9f74ebede4fbf85bab2b5174fc9158c340
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2026-02-11 16:56:00 +0800

    +double buffering

--- src/bin/caimg.rs
+++ src/bin/caimg.rs
@@ -24,22 +24,24 @@ struct Args {
     initial: Option<String>,
 }
 
-fn apply_rule(current: &[u8], rule: u8) -> Vec<u8> {
+fn apply_rule(current: &[u8], next: &mut [u8], rule: u8) {
     let len = current.len();
-    let mut next = vec![0; len];
-
     for i in 0..len {
         let left = current[(i + len - 1) % len];
         let center = current[i];
         let right = current[(i + 1) % len];
-
         let pattern = (left << 2) | (center << 1) | right;
-
-        if (rule >> pattern) & 1 == 1 {
-            next[i] = 1;
-        }
+        next[i] = (rule >> pattern) & 1;
     }
-    next
+}
+
+fn parse_initial(s: &str, width: usize) -> Vec<u8> {
+    s.chars()
+        .take(width)
+        .map(|c| if c == '1' { 1 } else { 0 })
+        .chain(std::iter::repeat(0))
+        .take(width)
+        .collect()
 }
 
 fn main() {
@@ -48,25 +50,31 @@ fn main() {
     let width = args.width as usize;
 
     // Initialize state: use provided string or a single '1' in the center
-    let mut state = vec![0u8; width];
-    if let Some(s) = args.initial {
-        state
-            .iter_mut()
-            .zip(s.chars())
-            .for_each(|(s, c)| *s = if c == '1' { 1 } else { 0 });
-    } else if args.width > 0 {
-        state[width / 2] = 1;
-    }
+    let mut state: Vec<u8> = args
+        .initial
+        .as_ref()
+        .map(|s| parse_initial(s, width))
+        .unwrap_or_else(|| {
+            let mut v = vec![0u8; width];
+            if width > 0 {
+                v[width / 2] = 1;
+            }
+            v
+        });
+    let mut next_state = vec![0u8; width];
 
     let mut img_buffer = ImageBuffer::new(width as u32, steps as u32);
+
     for y in 0..steps {
         for (x, &cell) in state.iter().enumerate() {
+            // use Luma<u8> for grayscale
             const BLACK: u8 = 0;
             const WHITE: u8 = 255;
             let pixel_val = if cell == 1 { WHITE } else { BLACK };
-            img_buffer.put_pixel(x as u32, y as u32, Luma([pixel_val])); // use Luma<u8> for grayscale
+            img_buffer.put_pixel(x as u32, y as u32, Luma([pixel_val])); 
         }
-        state = apply_rule(&state, args.rule);
+        apply_rule(&state, &mut next_state, args.rule);
+        std::mem::swap(&mut state, &mut next_state);
     }
 
     img_buffer
